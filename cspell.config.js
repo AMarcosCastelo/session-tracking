984300cf7653750d8ff74407e044012e22deb162
AMarcosCastelo <marcos-12marcos@hotmail.com>
2023-10-12 12:11:20 -0300

    chore: config prettier and husky

---
+++ cspell.config.js
@@ -0,0 +1,40 @@
+const vsCodeSettings = require('./.vscode/settings.json');
+
+module.exports = {
+  $schema:
+    'https://raw.githubusercontent.com/streetsidesoftware/cspell/main/cspell.schema.json',
+  version: '0.2',
+  language: 'en',
+  words: vsCodeSettings['cSpell.words'],
+  dictionaries: [
+    'companies',
+    'cpp',
+    'en_us',
+    'en-gb',
+    'fonts',
+    'fullstack',
+    'html',
+    'lorem-ipsum',
+    'node',
+    'npm',
+    'typescript',
+  ],
+  ignorePaths: [
+    'dist',
+    'node_modules',
+    'local',
+    'nx.json',
+    'package.json',
+    'pnpm-lock.yaml',
+    'tmp',
+    '.vscode',
+    '.gitignore',
+    'cspell.config.js',
+    '*.tfstate*',
+    '.env-files',
+    'apps/infra/terraform-cloud/src/aws-iam',
+  ],
+  allowCompoundWords: true,
+  useGitignore: true,
+  caseSensitive: true,
+};
